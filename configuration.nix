8a068820ccb93b08e2717182a5add4d09c25eceb
Jaage <jordanjhubbard@gmail.com>
2025-06-23 20:31:45 -0700

    No changes

--- configuration.nix
+++ configuration.nix
@@ -112,25 +112,6 @@
   services.xserver.xkb.variant = "";
   # Wooting keyboard detection
   hardware.wooting.enable = true;
-  services.udev.extraRules = ''
-    # Wooting One Legacy
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff01", TAG+="uaccess"
-
-    # Wooting One update mode
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2402", TAG+="uaccess"
-
-    # Wooting Two Legacy
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="ff02", TAG+="uaccess"
-
-    # Wooting Two update mode
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2403", TAG+="uaccess"
-
-    # Generic Wooting devices
-    SUBSYSTEM=="hidraw", ATTRS{idVendor}=="31e3", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTRS{idVendor}=="31e3", TAG+="uaccess"
-  '';
 
   # Security
   security = {
