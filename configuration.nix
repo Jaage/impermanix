59aa53b30b96d7cce530a366ae23750509ece981
Jaage <jordanjhubbard@gmail.com>
2025-06-25 17:20:11 -0700

    No changes

--- configuration.nix
+++ configuration.nix
@@ -180,8 +180,8 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # portalPackage =
-    #   inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   programs.zsh.enable = true;
