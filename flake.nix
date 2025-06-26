c3d32477ca27fbbdddb60d511b42378327850c7a
Jaage <jordanjhubbard@gmail.com>
2025-06-25 17:10:59 -0700

    No changes

--- flake.nix
+++ flake.nix
@@ -2,16 +2,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     impermanence.url = "github:nix-community/impermanence";
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    # hyprland.url = "github:hyprwm/Hyprland";
     nixvim.url = "github:Jaage/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [ ./configuration.nix inputs.impermanence.nixosModules.impermanence ];
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          inputs.impermanence.nixosModules.impermanence
+        ];
+      };
     };
-  };
 }
-
